954f3ae2ddf9608747dc681400ed2f7e0acb3916
Mauricio Gonzalez <mauriciogonzalez1256@gmail.com>
2026-05-17 23:01:19 -0400

    listado de perfiles dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ document.getElementById("searchButton").innerHTML = config.search;
 document.getElementById("semester").innerHTML = config.semester;
 
 document.getElementById("footerText").innerHTML = config.copyRight;
+
+
+const profilesContainer = document.getElementById("perfiles");
+const profilesCards = profiles;
+
+for (let i = 0; i < profilesCards.length; i++) {
+    const profile = profilesCards[i];
+    
+    profilesContainer.innerHTML += `<div id="${profile.ci}" class="perfil">  <picture> <source media="(max-width: 480px)" srcset="/${profile.ci}/${profile.ci}Small${profile.image_ext}"> <source media="(min-width: 481px)" srcset="/${profile.ci}/${profile.ci}Big${profile.image_ext}"> <img src="/${profile.ci}/${profile.ci}Big${profile.image_ext}" alt="${profile.name}"> </picture> <h2>${profile.name}</h2> </div> `;
+
+}
+
+profilesContainer.addEventListener("click", (event) => {
+    const profileCard = event.target.closest(".perfil");
+    if (profileCard) {
+        const ci = profileCard.id;
+        window.location.href = `profile.html?ci=${ci}`;
+    }
+});
